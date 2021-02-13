9e982e9c8c5a7eaeea3c7c3849fa43dfa3dc23f8
Hunter Van lear <huntervanlear@hunters-mbp.home>
2021-02-13 12:08:05 -0500

    added category and lang rows to News Source table

--- app/models.py
+++ app/models.py
@@ -6,6 +6,8 @@ class NewsSources(db.Model):
     name = db.Column(db.String(64), index=True, unique=True)
     full_url = db.Column(db.String(120), index=True, unique=True)
     formatted_url = db.Column(db.String(64), index=True, unique=True)
+    category = db.Column(db.String(64), index=True, unique=True)
+    language = db.Column(db.String(64), index=True, unique=True)
 
     def __repr__(self):
         return 'Source {}'.format(self.name)
