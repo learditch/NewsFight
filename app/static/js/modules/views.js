a49d47f6f586759e3b2f77d24c7c588b2719c8d3
Hunter <hvanlear@gmail.com>
2021-02-23 11:25:41 -0500

    default thumbnail and Author added

--- app/static/js/modules/views.js
+++ app/static/js/modules/views.js
@@ -1,3 +1,14 @@
+function checkForNull(storyAtt) {
+  return storyAtt ? storyAtt : (storyAtt = "N/A");
+}
+
+function checkForNullSrc(storySrc) {
+  return storySrc
+    ? storySrc
+    : (storySrc =
+        "https://getuikit.com/v2/docs/images/placeholder_200x100.svg");
+}
+
 export default {
   generateStoryMarkup: (story) => {
     return `
@@ -5,12 +16,18 @@ export default {
         <div class="card">
           <div class="card-body">
             <p class = 'story-score'>Objectivity Score: 4.6</p>
-            <img class="story-thumbnail" src="${story.urlToImage}" alt="">
+            <img class="story-thumbnail" src="${checkForNullSrc(
+              story.urlToImage
+            )}"  alt="">
             <h4 class ='story-title'>${story.title}</h4>
             <p class="lead story-description">${story.description}</p>
-            <p class= 'story-author'>Author : ${story.author}</p>
-            <p class= 'story-publishedAt'>Published At: ${story.publishedAt} </p>
-            <a class='story-url' href='${story.url}' target=”_blank">Full story</a>
+            <p class= 'story-author'>Author : ${checkForNull(story.author)}</p>
+            <p class= 'story-publishedAt'>Published At: ${
+              story.publishedAt
+            } </p>
+            <a class='story-url' href='${
+              story.url
+            }' target=”_blank">Full story</a>
           </div>
         </div>
       </li>
